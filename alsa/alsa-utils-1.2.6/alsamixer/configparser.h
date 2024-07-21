49b2834354739d8723c8d4a6bd19af33df96bdbd
cross-compilation <example@example.com>
2024-07-20 23:29:20 -0700

    fix(package): delete useless directories

--- alsa/alsa-utils-1.2.6/alsamixer/configparser.h
+++
@@ -1,9 +0,0 @@
-#ifndef CONFIGPARSER_H_INCLUDED
-#define CONFIGPARSER_H_INCLUDED
-
-#define CONFIG_DEFAULT ((const char*) 1)
-
-void parse_config_file(const char *file);
-void parse_default_config_file();
-
-#endif
--- alsa/ncurses-6.1/misc/run_tic.sed
+++
@@ -1,1 +0,0 @@
-s%/usr/share/tabset%/home/rpdzkj/sixth/ljswork/hisi/hi351xdv500/work/alsa/ncurses-6.1/install/share/tabset%g
