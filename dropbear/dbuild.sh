a4d05edd633793f219904ec0cf4a62e3318f929e
cross-compilation <example@example.com>
2024-07-21 00:07:42 -0700

    fix: delete some useless package

--- dropbear/dbuild.sh
+++ dropbear/dbuild.sh
@@ -1,18 +1,24 @@
 #!/bin/bash
 
 
-export PATH=/home/rpdzkj/sixth/ljswork/hisi/hi351xdv500/cross-compile/aarch64-v01c01-linux-gnu-gcc/bin:$PATH
-export CROSS_COMPILE=aarch64-linux-gnu
-export CC=aarch64-linux-gnu-gcc
+CMD=`realpath $0`
+COMMON_DIR=`dirname $CMD`
+
+export PATH=${COMMON_DIR}/../cross-compilation/gcc-arm-10.3-2021.07-x86_64-aarch64-none-linux-gnu/bin:$PATH
+export CROSS_COMPILE=aarch64-none-linux-gnu-
+export CROSS=aarch64-none-linux-gnu-
+export CXX=aarch64-none-linux-gnu-g++
+export AR=aarch64-none-linux-gnu-ar
+export RANLIB=aarch64-none-linux-gnu-ranlib
+export STRIP=aarch64-none-linux-gnu-strip
+export LD=aarch64-none-linux-gnu-ld
+export CC=aarch64-none-linux-gnu-gcc
 
 cd dropbear-2020.81/
 
-if [ -d dropbear-install ];then
-	rm -rf dropbear-install
-	mkdir dropbear-install
-fi
-
-./configure --prefix=`pwd`/dropbear-install --with-zlib=`pwd`/../zlib-1.2.8/zlib-install CC=aarch64-linux-gnu-gcc --host=linux-arch64
+./configure --prefix=`pwd`/install \
+	--with-zlib=${COMMON_DIR}/../zlib/zlib-1.2.11/install \
+	--host=aarch64-none-linux-gnu
 #make PROGRAMS="dropbear dbclient dropbearkey dropbearconvert scp"
 #make PROGRAMS="dropbear dbclient dropbearkey dropbearconvert scp" install
 make clean
