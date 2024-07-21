d45cc8c955c12f00096c15de4e84746c0e8bb677
cross-compilation <example@example.com>
2024-07-20 23:30:57 -0700

    fix(alsa): modify alsa script

--- alsa/alsalibbuild.sh
+++ alsa/alsalibbuild.sh
@@ -1,8 +1,22 @@
 #!/bin/bash
 
 
-export PATH=/home/rpdzkj/sixth/ljswork/hisi/hi351xdv500/cross-compile/aarch64-v01c01-linux-gnu-gcc/bin:$PATH
-export CC=aarch64-linux-gnu-gcc
+CMD=`realpath $0`
+COMMON_DIR=`dirname $CMD`
+
+
+export PATH=${COMMON_DIR}/../automake/automake-1.16/install/bin:${COMMON_DIR}/../cross-compilation/gcc-arm-10.3-2021.07-x86_64-aarch64-none-linux-gnu/bin:$PATH
+
+
+export CROSS_COMPILE=aarch64-none-linux-gnu-
+export CROSS=aarch64-none-linux-gnu-
+export CXX=aarch64-none-linux-gnu-g++
+export AR=aarch64-none-linux-gnu-ar
+export RANLIB=aarch64-none-linux-gnu-ranlib
+export STRIP=aarch64-none-linux-gnu-strip
+export LD=aarch64-none-linux-gnu-ld
+export CC=aarch64-none-linux-gnu-gcc
+
 
 cd alsa-lib-1.2.6.1/
 
@@ -10,7 +24,7 @@ if [ ! -d install ];then
 	mkdir install
 fi
 ./configure \
---host=aarch64-linux-gnu \
+--host=aarch64-none-linux-gnu \
 --prefix=`pwd`/install \
 --with-configdir=`pwd`/install/arm_alsalib_config
 
--- alsa/alsautilsbuild.sh
+++ alsa/alsautilsbuild.sh
@@ -1,20 +1,20 @@
 #!/bin/bash
 
 
-export PATH=/home/rpdzkj/sixth/ljswork/hisi/hi351xdv500/cross-compile/aarch64-v01c01-linux-gnu-gcc/bin:$PATH
-export CC=aarch64-linux-gnu-gcc
+CMD=`realpath $0`
+COMMON_DIR=`dirname $CMD`
 
+export PATH=${COMMON_DIR}/../cross-compilation/gcc-arm-10.3-2021.07-x86_64-aarch64-none-linux-gnu/bin:$PATH
+export CROSS_COMPILE=aarch64-none-linux-gnu-
+export CROSS=aarch64-none-linux-gnu-
+export CXX=aarch64-none-linux-gnu-g++
+export AR=aarch64-none-linux-gnu-ar
+export RANLIB=aarch64-none-linux-gnu-ranlib
+export STRIP=aarch64-none-linux-gnu-strip
+export LD=aarch64-none-linux-gnu-ld
+export CC=aarch64-none-linux-gnu-gcc
 
-
-export CROSS_COMPILE=aarch64-linux-gnu-
-export CROSS=aarch64-linux-gnu-
-export CXX=aarch64-linux-gnu-g++
-export AR=aarch64-linux-gnu-ar
-export RANLIB=aarch64-linux-gnu-ranlib
-export STRIP=aarch64-linux-gnu-strip
-export LD=aarch64-linux-gnu-ld
-
-
+export PKG_CONFIG_PATH=$PKG_CONFIG_PATH:${COMMON_DIR}/../ncursesw/ncurses-6.1/install/lib64/pkgconfig:${COMMON_DIR}/alsa-lib-1.2.6.1/install/lib/pkgconfig
 
 cd alsa-utils-1.2.6/
 
@@ -24,18 +24,19 @@ fi
 
 
 
-#./configure \
-#--host=aarch64-linux-gnu \
-#--prefix=`pwd`/install \
-#--with-systemdsystemunitdir=`pwd`/install/systemd  \
-#--with-udev-rules-dir=`pwd`/install/udev-rules \
-#--with-alsa-inc-prefix=/home/rpdzkj/sixth/ljswork/hisi/hi351xdv500/work/alsa/alsa-lib-1.2.6.1/install/include/ \
-#--with-alsa-prefix=/home/rpdzkj/sixth/ljswork/hisi/hi351xdv500/work/alsa/alsa-lib-1.2.6.1/install/lib/ \
-#--disable-xmlto \
-#NCURSESW_CFLAGS="`/home/rpdzkj/sixth/ljswork/hisi/hi351xdv500/work/alsa/ncurses-6.1/install/bin/ncursesw6-config --cflags`" \
-#NCURSESW_LIBS="`/home/rpdzkj/sixth/ljswork/hisi/hi351xdv500/work/alsa/ncurses-6.1/install/bin/ncursesw6-config --libs`"
+./configure \
+--host=aarch64-none-linux-gnu \
+--prefix=`pwd`/install \
+--with-systemdsystemunitdir=`pwd`/install/systemd  \
+--with-udev-rules-dir=`pwd`/install/udev-rules \
+--with-alsa-inc-prefix=${COMMON_DIR}/alsa-lib-1.2.6.1/install/include \
+--with-alsa-prefix=${COMMON_DIR}/alsa-lib-1.2.6.1/install/lib \
+--disable-xmlto \
+NCURSESW_CFLAGS="`${COMMON_DIR}/../ncursesw/ncurses-6.1/install/bin/ncursesw6-config --cflags`" \
+NCURSESW_LIBS="`${COMMON_DIR}/../ncursesw/ncurses-6.1/install/bin/ncursesw6-config --libs`"
 
+#CFLAGS="-I${COMMON_DIR}/alsa-lib-1.2.6.1/install/include" \
+#CPPFLAGS="-L${COMMON_DIR}/alsa-lib-1.2.6.1/install/lib" \
 
-make clean -j16
-make -j16
+make -j${nproc}
 make install
