6da7ae3f1941f14cc0ad5b4bd2553f371a72ce59
cross-compilation <example@example.com>
2024-07-20 23:33:03 -0700

    feat(automake): add automake-1.16 cross-compilation

---
+++ automake/automakebuild.sh
@@ -0,0 +1,25 @@
+#!/bin/bash -x
+
+CMD=`realpath $0`
+COMMON_DIR=`dirname $CMD`
+
+export PATH=${COMMON_DIR}/../cross-compilation/gcc-arm-10.3-2021.07-x86_64-aarch64-none-linux-gnu/bin:$PATH
+export CROSS_COMPILE=aarch64-none-linux-gnu-
+export CROSS=aarch64-none-linux-gnu-
+export CXX=aarch64-none-linux-gnu-g++
+export AR=aarch64-none-linux-gnu-ar
+export RANLIB=aarch64-none-linux-gnu-ranlib
+export STRIP=aarch64-none-linux-gnu-strip
+export LD=aarch64-none-linux-gnu-ld
+export CC=aarch64-none-linux-gnu-gcc
+cd automake-1.16/
+
+
+./configure --host=aarch64-none-linux-gnu \
+	--prefix=`pwd`/install \
+	--target=aarch64-none-linux-gnu 
+
+make -j${nproc}
+make install
+
+
