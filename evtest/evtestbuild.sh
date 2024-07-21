71c15119c2ee6e1373522a9b3b4b475d9377a17d
cross-compilation <example@example.com>
2024-07-21 02:56:13 -0700

    fix(evtest): delete useless src

--- evtest/evtestbuild.sh
+++ evtest/evtestbuild.sh
@@ -1,19 +1,22 @@
 #!/bin/bash
 
-export PATH=/home/rpdzkj/sixth/ljswork/hisi/hi351xdv500/cross-compile/aarch64-v01c01-linux-gnu-gcc/bin:$PATH
+CMD=`realpath $0`
+COMMON_DIR=`dirname $CMD`
 
+export PATH=${COMMON_DIR}/../cross-compilation/gcc-arm-10.3-2021.07-x86_64-aarch64-none-linux-gnu/bin:$PATH
 
 cd  evtest-evtest-1.34/
-if [ ! -d install ];then 
-	mkdir install
+
+if [ ! -f configure ];then
+	./autogen.sh
 fi
+
 ./configure --prefix=`pwd`/install \
 	--host=aarch64-linux-gnu \
 	CC=aarch64-linux-gnu-gcc
 
 	
 make  -j16
-
 make install
 
 
